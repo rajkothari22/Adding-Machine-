3b941893cc20c412a919e1b864be54e6d1031b96
damushah <damushah@10.180.37.127>
2019-10-09 19:47:10 -0700

    Completed the code

--- CSE 360 Assignments/src/cse360assign2/AddingMachine.java
+++ CSE 360 Assignments/src/cse360assign2/AddingMachine.java
@@ -3,29 +3,34 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total ; 
+	private String temp ; 
 	
 	public AddingMachine() {
 		total = 0 ; //not needed - included for clarity 
+		temp = "0" ; 
 	}
 	
 	public int getTotal() {
-		return 0 ; 
+		return total ; 
 	}
 	
 	public void add(int value) {
-		
+		temp += " + " + Integer.toString(value) ; 
+		total = total + value ; 
 	}
 	
 	public void subtract(int value) {
-		
+		temp += " - " + Integer.toString(value) ; 
+		total = total - value ; 
 	}
 	
 	public String toString() {
-		return "" ; 
+		return temp ; 
 	}
 	
 	public void clear() {
-		
+		total = 0 ; 
+		temp = "" ; 
 	}
 	
 }
